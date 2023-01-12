fee46f25a842595e80bebc977f187b8ad2dfb78a
Muges <muges.gaming@gmail.com>
2023-01-12 12:41:07 +1100

    test 1

--- PowerShell/SetIPStatic.ps1
+++ PowerShell/SetIPStatic.ps1
@@ -1,5 +1,8 @@
 # Get the current IP address configuration for the LAN adapter
-$currentIP = (Get-NetIPConfiguration | Where-Object {$_.DNSSuffix -eq "hayball.local"}).IPv4Address
+$currentIP = (Get-NetIPConfiguration | Where-Object {$_.NetProfile.Name -eq "hayball.local"}).IPv4Address
+$currentSUBNET = (get-wmiobject -class win32_networkadapterconfiguration | where-object {$_.DNSDomain -eq "hayball.local"}).ipsubnet | select-object -first 1
 
 # Set the IP address, subnet mask, and default gateway
-New-NetIPAddress -InterfaceIndex $currentIP.InterfaceIndex -IPAddress $currentIP.IPAddress -PrefixLength $currentIP.PrefixLength -DefaultGateway $currentIP.DefaultGateway
+netsh interface ip set address name="$currentIP.InterfaceAlias" static $currentIP.IPv4Address $currentSUBNET $currentIP.DefaultGateway
+netsh interface ip add dns name="$currentIP.InterfaceAlias" 10.0.0.5
+netsh interface ip add dns name="$currentIP.InterfaceAlias" 10.0.0.3 index=2
